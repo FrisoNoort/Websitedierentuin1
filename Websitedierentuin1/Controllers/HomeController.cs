cf7677d74f7ed366f5050dc69d5cedd4ff18aab0
Friso scholing <111106@st-maartenscollege.nl>
2023-03-20 11:00:48 +0100

    Productdetail pagina aangemaakt en in nav bar gezet. route weggehaald.

--- Websitedierentuin1/Controllers/HomeController.cs
+++ Websitedierentuin1/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace Websitedierentuin1.Controllers
             return View();
         }
 
+        public IActionResult Productdetails()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
