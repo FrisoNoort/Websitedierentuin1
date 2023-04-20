7f6e2116b681b13f51ca302688e146eb63009dd7
110934 <110934@SMC-LL-126-17>
2023-04-20 15:38:05 +0200

    Contact pagina mooier gemaakt met css, Errormessages en Labels etc.

--- Websitedierentuin1/Models/Person.cs
+++ Websitedierentuin1/Models/Person.cs
@@ -5,13 +5,23 @@ namespace Websitedierentuin1.Models
     public class Person
     {
         [Required(ErrorMessage = "Gelieve uw voornaam in te vullen")]
+        [Display(Name = "Voornaam")]
         public string voornaam { get; set; }
-        [Required(ErrorMessage = "Gelieve uw achternaam in te vullen")]
+
+        [Required(ErrorMessage = "Achternaam is een verplicht veld")]
+        [Display(Name = "Achternaam")]
         public string achternaam { get; set; }
-        [EmailAddress(ErrorMessage = "Emailadres is verplicht")]
+
+        [Required(ErrorMessage = "Emailadres is verplicht")]
+        [EmailAddress(ErrorMessage = "Geen geldig email adres")]
         public string email { get; set; }
+
         public string telefoonnummer { get; set; }
+
         public string adres { get; set; }
+
+        [Required(ErrorMessage = "Bericht is verplicht")]
+        [Display(Name = "Bericht")]
         public string beschrijving { get; set; }
     }
 }
