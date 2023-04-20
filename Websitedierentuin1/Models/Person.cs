c6ee5dbda3a4f2ce301a2d3730458134ab132dc8
110934 <110934@SMC-LL-126-17>
2023-04-20 12:00:04 +0200

    Contact pagina required toegevoegd

--- Websitedierentuin1/Models/Person.cs
+++ Websitedierentuin1/Models/Person.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Websitedierentuin1.Models
 {
     public class Person
     {
+        [Required]
         public string voornaam { get; set; }
+        [Required]
         public string achternaam { get; set; }
+        [Required]
         public string email { get; set; }
         public string telefoonnummer { get; set; }
         public string adres { get; set; }
